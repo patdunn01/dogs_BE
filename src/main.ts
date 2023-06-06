0488bb3a3284c8126f6299eee575422441589261
Patrick Dunn <patdunn01@hotmail.co.uk>
2023-06-06 15:56:42 +0100

    changes cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { cors } from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cors());
+  app.enableCors();
 
   await app.listen(3000);
 }
+
 bootstrap();
